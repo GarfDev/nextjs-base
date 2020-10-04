2106c82d056ce25031ab93f8f0aa6aef7c0d4ca9
Nguyen Gia Tuan <tuan.nguyen@hubble.sg>
2020-10-04 13:44:59 +0700

    Fixing bug for production build

--- store/constants.ts
+++ store/constants.ts
@@ -1,7 +1,5 @@
 import { cartInitialState } from "./core/cart/reducer";
-import { sessionInitialState } from "./core/session/reducer";
 
 export const applicationInitialState = {
   cart: cartInitialState,
-  session: sessionInitialState,
 };
--- store/types.ts
+++ store/types.ts
@@ -1,11 +1,9 @@
 import { Cart } from "./core/cart/types";
-import { SessionType } from "./core/session/types";
 import { ThemingType } from "./core/theming/types";
 import { WrapperType } from "./core/wrapper/types";
 
 export interface ApplicationRootState {
   cart: Cart;
   wrapper: WrapperType;
-  session: SessionType;
   theming: ThemingType;
 }
